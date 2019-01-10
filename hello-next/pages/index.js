fd576e6dc32d92b140716fdfdb76512136d980f0
Andrew Miracle <me@andrewmiracle.com>
2019-01-10 16:04:05 +0100

    Next Tutorial Navigate between pages

--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -1,7 +1,24 @@
+// Import the Next Link API
+import Link from "next/link"
+
 const Index = () => (
 	<div>
-	 <p>Hello Next.js</p>
-	<a href="/about">Take me About</a>
+	 <h2>Hello Next.js</h2>
+	  <a href="/about">About Link using HREF</a>
+	<br />
+
+	<Link href="/about" style={{ fontSize: 40, fontWeight: 700 }}>
+	 <a>About Page Next/Link</a>
+	</Link>
+
+	<Link href="/about">
+	 <button>Goto about page using button on Next/Link</button>
+	</Link>
+
+	<Link href="/about">
+	 <p style={{ cursor: "pointer" }} >A Next/Link can accept anything that works with an onClick</p>
+	</Link>
+
 	</div>
 )
 
