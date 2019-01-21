a1d9e2ed17dad35135d52e566570fbd904ebcebf
Andrew Miracle <me@andrewmiracle.com>
2019-01-21 23:24:09 +0100

    redux dispatch and store

--- redux/mead/src/playground/redux-101.js
+++ redux/mead/src/playground/redux-101.js
@@ -1,11 +1,48 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+ switch (action.type) {
+  case 'INCREMENT':
+    return {
+      count: state.count + 11
+    };
+  
+  case 'RESET':
+    return {
+      count: state.count = 0
+    };
+  
+    case 'DECREMENT': 
+    return {
+      count: state.count - 1
+    }
+    default: 
+    return state
+ }
+ 
 });
 
 console.log(store.getState());
 
 // Actions
 // I'd like to increment the count
+// Implement dispatch() function from redux This is passed as an argument on a store to dispatch an action type
+store.dispatch({
+  type: "INCREMENT"
+});
+
+store.dispatch({
+  type: "INCREMENT"
+});
+
+store.dispatch({
+  type: "RESET"
+});
+
+store.dispatch({
+  type: "DECREMENT"
+});
+
 // I'd like to reset the count to zero
+
+console.log(store.getState());
