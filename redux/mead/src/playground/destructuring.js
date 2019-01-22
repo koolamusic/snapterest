76c66ac4ef1140f8b30606f58d3e29a06fd33b99
Andrew Miracle <me@andrewmiracle.com>
2019-01-22 11:47:12 +0100

    object destructuring

---
+++ redux/mead/src/playground/destructuring.js
@@ -0,0 +1,37 @@
+import { normalize } from "path";
+
+const person = {
+  name: 'Andrew',
+  age: 36,
+  location: {
+    city: 'Lagos',
+    temp: 92,
+    age: 43
+  }
+}
+
+const { name = "Ruddy Mendy", age } = person;
+
+
+console.log(`${name} is ${age}`)
+
+
+// Desc person.location
+const { city, temp, age: lAge } = person.location;
+console.log(`${city} had been aged ${lAge} towards ${temp}`)
+
+
+// Desc for book club
+const book = {
+  title: 'Ego is the Enemy',
+  author: 'Ryan Holiday',
+  publisher: {
+    name: 'Penguin' 
+  }
+};
+
+let { name: publisherName = 'Self Published'} = book.publisher
+console.log(publisherName); // Penguin(valid name), Self-Published
+
+
+// 
