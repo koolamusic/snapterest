7175cf5e995112f33bf47b8a73c2804f65f97d49
Andrew Miracle <me@andrewmiracle.com>
2019-01-24 21:48:52 +0100

    Redux combine reducer functions

--- redux/mead/src/playground/redux-expensify.js
+++ redux/mead/src/playground/redux-expensify.js
@@ -1,5 +1,53 @@
 import { createStore, combineReducers } from 'redux';
 
+// ACTIONS
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+
+// Expense Reducer
+const expenseReducerDefaultState = []
+const expenseReducer = (state = expenseReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+    return state;
+  } 
+}
+
+// Filter Reducer
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+}
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+      default:
+      return state;
+    }
+  }
+
+
+// create a global store for our reducers and combine them using Redux combineReducers Function
+const store = createStore(combineReducers({
+  expenses: expenseReducer,
+  filters: filtersReducer
+})
+);
+
+
+// console the state of the store reducer
+console.log(store.getState());
+
+
+
 const demoState = {
   expenses: [{
     id: 'poijasdfhwer',
