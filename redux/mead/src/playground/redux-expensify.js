6d1f5363c2b8a82b89e2b802d2e39956ef7494d1
Andrew Miracle <me@andrewmiracle.com>
2019-01-25 11:03:20 +0100

    Reducer Functions

--- redux/mead/src/playground/redux-expensify.js
+++ redux/mead/src/playground/redux-expensify.js
@@ -1,52 +1,120 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
-// ACTIONS
 // ADD_EXPENSE
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+});
+
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+});
+
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+});
+
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
 // SET_END_DATE
 
+// Expenses Reducer
 
-// Expense Reducer
-const expenseReducerDefaultState = []
-const expenseReducer = (state = expenseReducerDefaultState, action) => {
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [
+        ...state,
+        action.expense
+      ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return expense;
+        };
+      });
     default:
-    return state;
-  } 
-}
+      return state;
+  }
+};
 
-// Filter Reducer
+// Filters Reducer
+
 const filtersReducerDefaultState = {
   text: '',
   sortBy: 'date',
   startDate: undefined,
   endDate: undefined
-}
+};
+
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
-    switch (action.type) {
-      default:
+  switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      };
+    default:
       return state;
-    }
   }
+};
 
+// Store creation
 
-// create a global store for our reducers and combine them using Redux combineReducers Function
-const store = createStore(combineReducers({
-  expenses: expenseReducer,
-  filters: filtersReducer
-})
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
 );
 
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-// console the state of the store reducer
-console.log(store.getState());
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
 
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
 
 const demoState = {
   expenses: [{
@@ -63,4 +131,3 @@ const demoState = {
     endDate: undefined
   }
 };
-
