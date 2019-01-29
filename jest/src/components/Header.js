e784a00f1b57673067672ce882baef56b9e2a322
Andrew Miracle <me@andrewmiracle.com>
2019-01-29 15:14:33 +0100

    Add Header navigation route

--- jest/src/components/Header.js
+++ jest/src/components/Header.js
@@ -4,9 +4,12 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    <NavLink to="/" activeClassName="is-active" exact={true}> Dashboard | </NavLink>
+    <NavLink to="/create" activeClassName="is-active"> Create Expense | </NavLink>
+    <NavLink to="/help" activeClassName="is-active"> Help | </NavLink>
+    <NavLink to="/persons" activeClassName="is-active"> Persons List | </NavLink>
+    <NavLink to="/personPost" activeClassName="is-active"> Persons Post | </NavLink>
+    <NavLink to="/personDel" activeClassName="is-active"> Persons Delete |</NavLink>
   </header>
 );
 
