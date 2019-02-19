c179cac92b9f71ca37c416f1c7df2a2cddfe58fa
Andrew Miracle <me@andrewmiracle.com>
2019-02-19 17:34:50 +0100

    update app.js

--- redux/mead/src/app.js
+++ redux/mead/src/app.js
@@ -12,9 +12,10 @@ const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill' }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
+// store.dispatch(setTextFilter('water'));
 
-const state = store.getState();
+const state = store.getState()
+console.log("===>", state.filters)
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
