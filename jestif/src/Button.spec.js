5531f6e6c96c6c42509d7c7cd4c9e211da68c8ff
Andrew Miracle <me@andrewmiracle.com>
2019-06-13 17:05:28 +0100

    initial test for button spec

---
+++ jestif/src/Button.spec.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+const Button = styled.button`
+  padding: 5px 10px;
+  background-color: green;
+  border: none;
+  color: white;
+`;
+export default Button;
